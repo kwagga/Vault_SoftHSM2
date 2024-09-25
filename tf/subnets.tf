c1505106e21cde25f61417357e7a3747c9fb91a2
kwagga <janroot@gmail.com>
2024-09-25 12:05:06 +0200

    Fixed subnet creation per AZ

--- tf/subnets.tf
+++ tf/subnets.tf
@@ -1,19 +1,28 @@
-resource "aws_subnet" "vault-softhsm-subnet" {
+resource "aws_subnet" "vault-softhsm-subnet_1" {
   cidr_block        = cidrsubnet(aws_vpc.vault-softhsm-vpc.cidr_block, 3, 1)
   vpc_id            = aws_vpc.vault-softhsm-vpc.id
   availability_zone = "eu-central-1a"
+  tags = {
+    Name = "${random_pet.name.id}-subnet-1"
+  }
 }
 
-resource "aws_subnet" "vault-softhsm-subnet" {
-  cidr_block        = cidrsubnet(aws_vpc.vault-softhsm-vpc.cidr_block, 3, 1)
+resource "aws_subnet" "vault-softhsm-subnet_2" {
+  cidr_block        = cidrsubnet(aws_vpc.vault-softhsm-vpc.cidr_block, 3, 2)
   vpc_id            = aws_vpc.vault-softhsm-vpc.id
   availability_zone = "eu-central-1b"
+  tags = {
+    Name = "${random_pet.name.id}-subnet-2"
+  }
 }
 
-resource "aws_subnet" "vault-softhsm-subnet" {
-  cidr_block        = cidrsubnet(aws_vpc.vault-softhsm-vpc.cidr_block, 3, 1)
+resource "aws_subnet" "vault-softhsm-subnet_3" {
+  cidr_block        = cidrsubnet(aws_vpc.vault-softhsm-vpc.cidr_block, 3, 3)
   vpc_id            = aws_vpc.vault-softhsm-vpc.id
   availability_zone = "eu-central-1c"
+  tags = {
+    Name = "${random_pet.name.id}-subnet-3"
+  }
 }
 
 resource "aws_route_table" "vault-softhsm-rtb" {
